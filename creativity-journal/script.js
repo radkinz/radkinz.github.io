935b7da0c845ce1825c65fea10ecfb24ebe1d05a
Ruby Adkins <radkins22@students.stab.org>
2021-11-09 01:45:05 -0500

    remove unneeded files

--- creativity-journal/script.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello 🌎");
-alert("Welcome to my creativity journal. FYI you can click on each project caption to see a subpage where the project's code will run. Thanks for coming, have a nice stay!")
